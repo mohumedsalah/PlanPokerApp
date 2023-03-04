9c785d65f34b60d12387dcea4391c461b317c1ab
salah <salahmuhammed@hammoq.com>
2023-03-04 16:49:29 +0200

    active main action

--- apps/fastify-server/src/app/poker/poker.gateway.ts
+++ apps/fastify-server/src/app/poker/poker.gateway.ts
@@ -1,10 +1,45 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+	OnGatewayInit,
+	SubscribeMessage,
+	WebSocketGateway,
+	WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({ namespace: '/poker' })
-export class PokerGateway {
-	@SubscribeMessage('message')
-	handleMessage(client: any, payload: any): string {
-		console.log('comming to back end ..........');
-		return 'Hello world!';
+export class PokerGateway implements OnGatewayInit {
+	@WebSocketServer()
+	wss: Server;
+
+	private logger: Logger = new Logger('ChatGateway');
+
+	afterInit(server: any) {
+		this.logger.log('Initialized!');
+	}
+
+	@SubscribeMessage('joinRoom')
+	handleRoomJoin(client: Socket, room: string) {
+		console.log('xxxxx');
+		client.join(room);
+		client.emit('joinedRoom', room);
+	}
+
+	@SubscribeMessage('revertCards')
+	handleMessage(client: Socket, message: { sender: string; room: string }) {
+		console.log('eeeeeeeeeee');
+
+		console.log(JSON.stringify(message, null, 2));
+		this.wss.to(message.room).emit('revertedCards', message);
+	}
+
+	@SubscribeMessage('vote')
+	vote(
+		client: Socket,
+		message: { sender: string; room: string; rate: string }
+	) {
+		console.log('wwwwwwwwwwwwwwww');
+
+		this.wss.to(message.room).emit('voted', message);
 	}
 }
--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -7,16 +7,34 @@ import { Loading } from 'shared';
 import Routing from 'routes';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000/poker');
-
-const joinRoom = async () => {
-	console.log('xxxx');
-	const ret = socket.emit('message');
-
-	console.log(ret, 'xxxxxyyyyy');
-};
+const room = 'HelloFromOtherSide';
 
 function App() {
+	const socket = io('http://localhost:4000/poker');
+	const joinRoom = () => {
+		socket.emit('joinRoom', room);
+	};
+
+	const voteMessage = () => {
+		socket.emit('vote', { room, sender: 'mo', rate: 3 });
+	};
+
+	const revertCards = () => {
+		socket.emit('revertCards', { room, sender: 'mo' });
+	};
+
+	useEffect(() => {
+		socket.on('revertedCards', (data) => {
+			console.log('cards need to revert with data', data);
+		});
+		socket.on('voted', (data) => {
+			console.log('new vote for coming', data);
+		});
+
+		socket.on('joinedRoom', (data) => {
+			console.log('new one join for coming', data);
+		});
+	}, [socket]);
 	return (
 		<Suspense fallback={<Loading />}>
 			<Provider store={store}>
@@ -27,7 +45,21 @@ function App() {
 					className="mt-3 block bg-indigo-600 text-xl font-bold  text-white p-2"
 					onClick={joinRoom}
 				>
-					Hello from the other side
+					Join room
+				</button>
+
+				<button
+					className="mt-3 block bg-indigo-600 text-xl font-bold  text-white p-2"
+					onClick={voteMessage}
+				>
+					vote
+				</button>
+
+				<button
+					className="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+					onClick={revertCards}
+				>
+					revert cards
 				</button>
 			</Provider>
 		</Suspense>
